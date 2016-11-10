5bdb2df562813e6964aa216bddcb86afaa67c000
KevinLaw <kevinlaw@thinkgeo.com>
2016-11-10 11:15:41 +0800

    Build sample

---
+++ EcwSample/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.WinForms;
+
+namespace ThinkGeo.MapSuite.Samples
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
+
+            LayerOverlay overlay = new LayerOverlay();
+
+            EcwRasterLayer ecwRasterLayer = new EcwRasterLayer("../../App_Data/World.ecw");
+            overlay.Layers.Add(ecwRasterLayer);
+
+            winformsMap1.Overlays.Add(overlay);
+
+            ecwRasterLayer.Open();
+            winformsMap1.CurrentExtent = ecwRasterLayer.GetBoundingBox();
+
+            winformsMap1.Refresh();
+        }
+    }
+}
